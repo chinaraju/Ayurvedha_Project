34874acd0982ffe60e3405856ab986f7a8f2e887
CHINA RAJU SARAGADA <chinaraju8877@gmail.com>
2023-11-21 22:54:12 +0530

    first commit

---
+++ spring_docker/src/test/java/com/china/SpringDockerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.china;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+
+class SpringDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
